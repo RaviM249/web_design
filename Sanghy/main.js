3945319cc20434e3874963f0eddc5f062636b4c4
Ravi Kumar <53962323+RaviM249@users.noreply.github.com>
2020-12-06 07:59:49 +0530

    Add files via upload

---
+++ Sanghy/main.js
@@ -0,0 +1,38 @@
+var lefty=document.getElementById("lefty")
+function function1()
+{
+	lefty.remove();
+}
+var lefty2=document.getElementById("lefty2")
+function function3()
+{
+	lefty2.remove();
+}
+var lefty1=document.getElementById("lefty1")
+function function2()
+{
+	lefty1.remove();
+}
+
+
+var toggle1=document.getElementById("toggle1");
+function function4()
+{
+	if (toggle1.style.display==="none"){
+		toggle1.style.display="block";
+	}
+	else{
+		toggle1.style.display="none";
+	}
+}
+
+var toggle2=document.getElementById("toggle2")
+function function5()
+{
+		if (toggle2.style.display==="none"){
+		toggle2.style.display="block";
+	}
+	else{
+		toggle2.style.display="none";
+	}
+}
